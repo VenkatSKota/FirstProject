4304faaef3b7be1cf741448ecc8afb6c1170c827
VenkataSai Kota <vssk.kota@gmail.com>
2020-05-19 12:34:45 +0530

    Email Trigger Validation

--- src/com/git/firstproject/HelloWorld.java
+++ src/com/git/firstproject/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++) {
 			System.out.println("Hello World -- "+i);
-			System.out.println("*********");
 		}
 
 	}
